26f9ec401794d98089efa7acb753d8ae854a2dbe
Felix Lehmann <lehmannfelix77gmail.com>
2022-02-24 12:58:14 +0100

    load show category-data in a table

--- lib/widgets/Pages/sqflit_items_table.dart
+++ lib/widgets/Pages/sqflit_items_table.dart
@@ -85,7 +85,6 @@ void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   static const String _title = 'Flutter Code Sample';
 
   @override
@@ -94,7 +93,7 @@ class MyApp extends StatelessWidget {
       title: _title,
       home: Scaffold(
         appBar: AppBar(title: const Text(_title)),
-        body: MyStatelessWidget(),
+        body: const MyStatelessWidget(),
       ),
     );
   }
@@ -108,94 +107,215 @@ class MyStatelessWidget extends StatefulWidget {
 }
 
 class _MyStatelessWidgetState extends State<MyStatelessWidget> {
-  final Set<String> categorys = {'Käse', 'Butter', 'Milch', 'Brot'};
-  final Set<String> items = {
+  DbManager dbManager = DbManager.instance;
+
+  late Future<List<String>> _categoryNamesSet;
+
+  final List<String> categorys = ['Käse', 'Butter', 'Milch', 'Brot'];
+  final List<String> items = [
     'Gauda',
     'Markenbutter',
     'Vollmilch 3,8%',
     'Körnerbrot'
-  };
-  final List<TableRow> tableRows = [];
+  ];
 
   @override
   void initState() {
     super.initState();
-    tableRows.add(TableRow(children: [
-      TableCell(
-        verticalAlignment: TableCellVerticalAlignment.top,
-        child: Container(
-          height: 32,
-          width: 32,
-          color: Color.fromARGB(255, 255, 255, 255),
-          child: const Center(
-            child: Text(
-              'Kategorie',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-          ),
-        ),
-      ),
-      TableCell(
-        verticalAlignment: TableCellVerticalAlignment.top,
-        child: Container(
-          height: 32,
-          width: 32,
-          color: Color.fromARGB(255, 255, 255, 255),
-          child: const Center(
-            child: Text(
-              'Artikel',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-          ),
-        ),
-      )
-    ]));
-    debugPrint('_MyStatelessWidgetState _initState()');
-    for (String categoryElement in categorys) {
-      tableRows.add(TableRow(children: [
-        TableCell(
-          verticalAlignment: TableCellVerticalAlignment.top,
-          child: Container(
-            height: 32,
-            width: 32,
-            color: Colors.blue[100],
-            child: Center(child: Text(categoryElement)),
-          ),
-        ),
-        TableCell(
-          verticalAlignment: TableCellVerticalAlignment.top,
-          child: Container(
-            height: 32,
-            width: 32,
-            color: Colors.blue[100],
-            child: Center(child: Text(categoryElement)),
-          ),
-        ),
-      ]));
+    _categoryNamesSet = _loadData();
+    // _loadItems();
+  }
+
+  Future<List<String>> _loadData() async {
+    debugPrint('_loadData()');
+    List<Map<String, dynamic>> _categoryNamesListMap = [];
+    try {
+      debugPrint('try  dbManager.queryAllRows');
+      _categoryNamesListMap = await dbManager.queryAllRows(
+          tableName: dbManager.tableNameCategories);
+    } catch (e) {
+      debugPrint('load error e = ' + e.toString());
+      _categoryNamesListMap = [];
     }
+    List<String> categoryNamesSet = <String>[];
+
+    for (var element in _categoryNamesListMap) {
+      categoryNamesSet.add(element['category_name']);
+    }
+
+    if (_categoryNamesListMap.toString().contains('no such table')) {
+      debugPrint('error no such table');
+      return [];
+    }
+    debugPrint(
+        'categoryNamesListMap loadet = ' + _categoryNamesListMap.toString());
+
+    // 'no such table'
+    return categoryNamesSet;
+  }
+
+  List<TableRow> _getTableRows(
+      {required List<String> categorys, required List<String> items}) {
+    final List<TableRow> tableRows = [];
+
+    const TextStyle _headlineCellsTextStyle = TextStyle(
+        fontSize: 16, color: Colors.black, fontWeight: FontWeight.bold);
+    const TextStyle _cellTextStyle = TextStyle(
+        fontSize: 16, color: Colors.black, fontWeight: FontWeight.normal);
+
+    tableRows.add(
+      TableRow(
+        children: [
+          TableCell(
+            verticalAlignment: TableCellVerticalAlignment.top,
+            child: Container(
+              height: 32,
+              width: 32,
+              color: const Color.fromARGB(255, 255, 255, 255),
+              child: const Center(
+                child: Text(
+                  'Kategorie',
+                  style: _headlineCellsTextStyle,
+                ),
+              ),
+            ),
+          ),
+          TableCell(
+            verticalAlignment: TableCellVerticalAlignment.top,
+            child: Container(
+              height: 32,
+              width: 32,
+              color: const Color.fromARGB(255, 255, 255, 255),
+              child: const Center(
+                child: Text(
+                  'Artikel',
+                  style: _headlineCellsTextStyle,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+    for (int index = 0; index < categorys.length; index++) {
+      tableRows.add(
+        TableRow(
+          children: [
+            TableCell(
+              verticalAlignment: TableCellVerticalAlignment.top,
+              child: Container(
+                height: 32,
+                width: 32,
+                color: Colors.blue[100],
+                child: Center(
+                  child: Text(
+                    categorys.elementAt(index),
+                    style: _cellTextStyle,
+                  ),
+                ),
+              ),
+            ),
+            TableCell(
+              verticalAlignment: TableCellVerticalAlignment.top,
+              child: Container(
+                height: 32,
+                width: 32,
+                color: Colors.blue[100],
+                child: Center(
+                  child: Text(items.elementAt(index), style: _cellTextStyle),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+    return tableRows;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(
-            width: double.infinity,
-            child: Table(
-              border: TableBorder.all(),
-              columnWidths: const {
-                0: FractionColumnWidth(0.5),
-                1: FractionColumnWidth(0.5)
-              },
-              defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-              children: tableRows,
-            ),
-          ),
-        ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: FutureBuilder(
+          future: _categoryNamesSet,
+          builder: (context, snapshot) {
+            final Widget widget;
+            if (snapshot.hasData) {
+              var categorys = snapshot.data as List<String>;
+              final List<TableRow> tableRows =
+                  _getTableRows(categorys: categorys, items: categorys);
+              widget = Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Table(
+                  children: tableRows,
+                  border: TableBorder.all(),
+                ),
+              );
+            } else {
+              widget = const Center(
+                child: Text('Daten werden geladen...'),
+              );
+            }
+
+            return widget;
+          },
+        ),
       ),
     );
+    // return MaterialApp(
+    //   debugShowCheckedModeBanner: false,
+    //   home: FutureBuilder(
+    //     future: _categoryNamesSet,
+    //     builder: (context, snapshot) {
+    //       Widget widget;
+    //       if (snapshot.hasData) {
+    //         widget = Scaffold(
+    //             body: CategoryListPage(
+    //                 refreshFunction: _refreshData,
+    //                 categoryNames: snapshot.data as Set<Object>));
+    //       } else if (snapshot.hasError) {
+    //         widget = const Scaffold(
+    //             body: Center(
+    //                 child: Text('Error!',
+    //                     style: TextStyle(
+    //                         fontSize: 20,
+    //                         fontStyle: FontStyle.italic,
+    //                         color: Colors.red))));
+    //       } else {
+    //         widget = const Scaffold(
+    //             backgroundColor: Colors.grey,
+    //             body: Center(
+    //                 child: Text('Daten werden geladen...',
+    //                     style: TextStyle(
+    //                         fontSize: 20,
+    //                         fontStyle: FontStyle.italic,
+    //                         color: Colors.black54))));
+    //       }
+    //       return widget;
+    //     },
+    //   ),
+
+    //  Padding(
+    //   padding: const EdgeInsets.all(16.0),
+    //   child: Column(
+    //     crossAxisAlignment: CrossAxisAlignment.center,
+    //     children: [
+    //       SizedBox(
+    //         width: double.infinity,
+    //         child: Table(
+    //           border: TableBorder.all(),
+    //           columnWidths: const {
+    //             0: FractionColumnWidth(0.5),
+    //             1: FractionColumnWidth(0.5)
+    //           },
+    //           defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+    //           children: tableRows,
+    //         ),
+    //       ),
+    //     ],
+    //   ),
+    // )
   }
 }
