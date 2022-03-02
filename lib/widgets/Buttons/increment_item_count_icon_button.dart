8351c15f90644d942c6daaf75c263c36511bc83f
Felix Lehmann <lehmannfelix77gmail.com>
2022-03-02 19:24:08 +0100

    IncrementItemCountIconButton addet

---
+++ lib/widgets/Buttons/increment_item_count_icon_button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class IncrementItemCountIconButton extends StatelessWidget {
+  const IncrementItemCountIconButton(
+      {required this.itemId, required this.buttonPressed, Key? key})
+      : super(key: key);
+  final Function buttonPressed;
+  final int itemId;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.add_circle_rounded),
+      color: Colors.blue,
+      onPressed: () => buttonPressed,
+    );
+  }
+}
